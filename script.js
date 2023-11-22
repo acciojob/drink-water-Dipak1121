16da26f243ee25a5bb79cf256901753182dfb694
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-22 22:20:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let smallCups = document.querySelectorAll(".cup-small");
+for ( let i = 0; i < smallCups.length; i++ ){
+	smallCups[i].addEventListener("click", onClick);
+}
+
+function onClick(event){
+	event.target.classList.toggle("full");
+}
